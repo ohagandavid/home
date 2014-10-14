11a1550004d7a2b29b0c66a30ff6b7cd16c4b22f
David O'Hagan <ohagandavid@gmail.com>
2014-10-14 10:01:15 -0600

    First Commit

---
+++ .config/openbox/autostart.sh
@@ -0,0 +1,10 @@
+/home/david/.screenlayout/default.sh &
+compton -bGC &
+/home/david/.config/wallpapers/stitch.py &
+tilda /home/david/.config/tilda/config_0 &
+(sleep 1 && tilda /home/david/.config/tilda/config_1) &
+tint2 &
+dropboxd &
+pnmixer &
+xfce4-power-manager &
+conky &
