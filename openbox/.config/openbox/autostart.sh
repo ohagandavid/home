50bab41e56af4e26535e5770319a17385e4a45e0
David O'Hagan <ohagandavid@gmail.com>
2015-07-19 10:21:56 -0600

    Adding stow directories.

---
+++ openbox/.config/openbox/autostart.sh
@@ -0,0 +1,9 @@
+compton -bGC &
+#/home/david/.config/wallpapers/stitch.py &
+tilda /home/david/.config/tilda/config_0 &
+#(sleep 1 && tilda /home/david/.config/tilda/config_1) &
+tint2 -c /home/david/.config/tint2/Numix_tint2/red_taskbar/tint2rc &
+#dropboxd &
+xfce4-power-manager &
+conky -c /home/david/.config/conky/Conky-Spectro/Themes/Numix/Celsius/single-color/.conkyrc &
+synergy --daemon --no-tray --restart -c /home/david/._to_mocha &
